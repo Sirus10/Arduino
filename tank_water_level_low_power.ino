0cfceb992d7590fa67b2d4a30eefb891a4c5af39
Sirus10 <coindre_e@hotmail.fr>
2016-04-24 18:05:09 +0200

    Update tank_water_level_low_power.ino

--- tank_water_level_low_power.ino
+++ tank_water_level_low_power.ino
@@ -1,27 +1,28 @@
-/*****************************************************
-# This script aim at reporting tank water level
-# Using Attiny85 devices 
+/******************************************
+# This script aim at reporting tack water level
 # Site    : http://domotique.web2diz.net/
 # Detail  : http://domotique.web2diz.net/?p=596
 #
 # License : CC BY-SA 4.0
 #
-# This script use the x10rf and sleep library 
-# See source and reference here : 
+# This script use the x10rf library 
+# See source here : 
 # https://github.com/p2baron/x10rf
-# https://gist.github.com/JChristensen/5616922
 #
-# To troubleshoot on Arduino you can 
-# uncommend the Serial lines 
 #
-/******************************************************/
+/*******************************************/
 
 // including x10rf library
 #include <x10rf.h>
 #include <avr/sleep.h>
-#define BODS 7                     //BOD Sleep bit in MCUCR
-#define BODSE 2                    //BOD Sleep enable bit in MCUCR
+#include <avr/wdt.h>
 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 // define I/O PINS
 #define SENSOR_LEVEL_0_PIN 0    // SENSOR LEVEL 0
@@ -32,36 +33,36 @@
 
 x10rf myx10 = x10rf(RF_OUT,0,5);
 int level = 0;
+volatile boolean f_wdt = 1;
 
-
+// The setup 
 void setup() {
-// To display log on Arduino only
-// Serial.begin(9600);
+ADCSRA &= ~(1<<ADEN);// disable ADC (before power-off)
+  // SETING UP THE I/O PINS
+  pinMode(SENSOR_LEVEL_0_PIN, INPUT);
+  pinMode(SENSOR_LEVEL_1_PIN, INPUT);
+  pinMode(SENSOR_LEVEL_2_PIN, INPUT);
+  pinMode(SENSOR_LEVEL_3_PIN, INPUT);
+  pinMode(RF_OUT, OUTPUT);
 
-// Disable ADC 
-ADCSRA &= ~(1<<ADEN);
-
-// SETING UP THE I/O PINS
-pinMode(SENSOR_LEVEL_0_PIN, INPUT);
-pinMode(SENSOR_LEVEL_1_PIN, INPUT);
-pinMode(SENSOR_LEVEL_2_PIN, INPUT);
-pinMode(SENSOR_LEVEL_3_PIN, INPUT);
-pinMode(RF_OUT, OUTPUT);
-
-// MYX10 INITIALIZATION
-myx10.begin();          
-
+  // MYX10 INITIALIZATION
+  myx10.begin();          
+   setup_watchdog(9); // approximately 8 seconds sleep 
 }
 
-// THE MAIN LOOP
+// The loop
 void loop() {
 
 get_and_sent_water_level();
 
-// Go to sleep in low power mode with x * 8s (here 80s)
-	for (int i=0; i<10; i++){
-		goToSleep(); // sleep for 8s
-	}
+delay(100);
+
+// Sleep for 8 s  8 x 75 = 600 s = 10m
+for (int i=0; i<75; i++){
+      system_sleep();
+}
+delay(100);
+
 }
 
 
@@ -71,11 +72,11 @@ get_and_sent_water_level();
 */
 int get_and_sent_water_level(){
 
-// INTERNAL PULL-UP DESABLING TO AVOID ELECTROLYSE 
-digitalWrite(SENSOR_LEVEL_0_PIN, LOW); 
-digitalWrite(SENSOR_LEVEL_1_PIN, LOW); 
-digitalWrite(SENSOR_LEVEL_2_PIN, LOW); 
-digitalWrite(SENSOR_LEVEL_3_PIN, LOW); 
+// INTERNAL PULL-UP ENABLING TO BE ABLE TO READ 
+digitalWrite(SENSOR_LEVEL_0_PIN, HIGH); 
+digitalWrite(SENSOR_LEVEL_1_PIN, HIGH); 
+digitalWrite(SENSOR_LEVEL_2_PIN, HIGH); 
+digitalWrite(SENSOR_LEVEL_3_PIN, HIGH); 
 
 delay(100);
 /*
@@ -88,56 +89,60 @@ Serial.println(digitalRead(SENSOR_LEVEL_2_PIN));
 Serial.print(" LEVEL  : ");
 Serial.println(digitalRead(SENSOR_LEVEL_3_PIN));  
 */
-
-// calculate the water level value: 
 int level = (1-digitalRead(SENSOR_LEVEL_0_PIN)) + (1-digitalRead(SENSOR_LEVEL_1_PIN)) + (1-digitalRead(SENSOR_LEVEL_2_PIN)) + (1-digitalRead(SENSOR_LEVEL_3_PIN)) ;
-
 /*
 Serial.print(" LEVEL OF WATER  : ");
 Serial.println(level);  
 */
 
 // INTERNAL PULL-UP DESABLING TO AVOID ELECTROLYSE 
-digitalWrite(SENSOR_LEVEL_0_PIN, HIGH); 
-digitalWrite(SENSOR_LEVEL_1_PIN, HIGH); 
-digitalWrite(SENSOR_LEVEL_2_PIN, HIGH); 
-digitalWrite(SENSOR_LEVEL_3_PIN, HIGH); 
+digitalWrite(SENSOR_LEVEL_0_PIN, LOW); 
+digitalWrite(SENSOR_LEVEL_1_PIN, LOW); 
+digitalWrite(SENSOR_LEVEL_2_PIN, LOW); 
+digitalWrite(SENSOR_LEVEL_3_PIN, LOW); 
 
 // SENDING THE LEVEL OVER RF
 myx10.RFXmeter(12,0,level);
-delay(100);
+
 }
 
-/*
-      FUNCTION
-      goToSleep
-*/
-void goToSleep(void)
-{
-    byte adcsra, mcucr1, mcucr2;
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    sleep_enable();
-    MCUCR &= ~(_BV(ISC01) | _BV(ISC00));      //INT0 on low level
-    GIMSK |= _BV(INT0);                       //enable INT0
-    adcsra = ADCSRA;                          //save ADCSRA
-    ADCSRA &= ~_BV(ADEN);                     //disable ADC
-    cli();                                    //stop interrupts to ensure the BOD timed sequence executes as required
-    mcucr1 = MCUCR | _BV(BODS) | _BV(BODSE);  //turn off the brown-out detector
-    mcucr2 = mcucr1 & ~_BV(BODSE);            //if the MCU does not have BOD disable capability,
-    MCUCR = mcucr1;                           //  this code has no effect
-    MCUCR = mcucr2;
-    sei();                                    //ensure interrupts enabled so we can wake up again
-    sleep_cpu();                              //go to sleep
-    sleep_disable();                          //wake up here
-    ADCSRA = adcsra;                          //restore ADCSRA as it was before
+
+
+
+
+// set system into the sleep state 
+// system wakes up when wtchdog is timed out
+void system_sleep() {
+  cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System sleeps here
+  sleep_disable();                     // System continues execution here when watchdog timed out 
+ // sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
 }
 
-/*
-      FUNCTION
-	   external interrupt 0 wakes the MCU
-*/
-ISR(INT0_vect)
-{
-    GIMSK = 0;                     //disable external interrupts (only need one to wake up)
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(int ii) {
+
+  byte bb;
+  int ww;
+  if (ii > 9 ) ii=9;
+  bb=ii & 7;
+  if (ii > 7) bb|= (1<<5);
+  bb|= (1<<WDCE);
+  ww=bb;
+
+  MCUSR &= ~(1<<WDRF);
+  // start timed sequence
+  WDTCR |= (1<<WDCE) | (1<<WDE);
+  // set new watchdog timeout value
+  WDTCR = bb;
+  WDTCR |= _BV(WDIE);
+}
+  
+// Watchdog Interrupt Service / is executed when watchdog timed out
+ISR(WDT_vect) {
+  f_wdt=1;  // set global flag
 }
 
